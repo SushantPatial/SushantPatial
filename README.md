69d9e97339b652a66dd47f1c4e28ea428f2f4148
Sushant Patial <sushantpatial18@gmai.com>
2022-01-27 16:38:38 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+## Hey there I'm Sushant 👋
+
+A Web Development Enthusiast 💻
+
+### About me:
+- 👨🏽‍💻 I’m currently an IT Undergrad student at ADGITM, GGSIPU.
+- 🌱 I'm currently exploring Web Development and Java.
+- 🤔 My interest lies in Front End Development.
+- 💬 Ask me about anything, I am happy to help.
+- 📫 Email me at: sushantpatial18@gmail.com
+- ⚡ Fun fact: I love cooking!
+
+### Languages and Frameworks
+<p>
+  <img src="https://github.com/abranhe/programming-languages-logos/blob/master/src/java/java_64x64.png" alt="Java" height="30" style="vertical-align:top; margin:4px">
+  <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/cpp/cpp.png" alt="CPP" height="30" style="vertical-align:top; margin:4px">
+   <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/python/python.png" alt="Python" height="30" style="vertical-align:top; margin:4px">
+   <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/visual-studio-code/visual-studio-code.png" alt="VS Code" height="30" style="vertical-align:top; margin:4px">
+</p>
+<p>
+  <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png" alt="HTML" height="30" style="vertical-align:top; margin:4px">
+  <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png" alt="css" height="30" style="vertical-align:top; margin:4px">
+  <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png" alt="Javascript" height="30" style="vertical-align:top; margin:4px">
+  <img src="https://github.com/devicons/devicon/blob/master/icons/jquery/jquery-plain.svg" alt="JQuery" height="30" style="vertical-align:top; margin:4px">
+  <img src="https://camo.githubusercontent.com/63821b655194bb17978c43f068fc6b6bfa4ee58d956a6215b6ae0b17dd79ed7e/68747470733a2f2f696d672e69636f6e73382e636f6d2f636f6c6f722f32782f626f6f7473747261702e706e67" alt="Bootstrap" height="30" style="vertical-align:top; margin:4px">
+</p>
+<p>
+  <img src="https://github.com/abranhe/programming-languages-logos/blob/master/src/php/php_64x64.png" alt="PHP" height="30" style="vertical-align:top; margin:4px">
+  <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mysql/mysql.png" alt="MySQL" height="30" style="vertical-align:top; margin:4px">
+</p>
+
+<hr>
+
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=SushantPatial&theme=tokyonight)
+
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,141 @@
+/* AOS */
+AOS.init({
+  once: true
+})
+
+/* Hero type effect */
+ new Typed('#typed',{
+  strings : ['A Web Developer','An Engineering Student', 'An Open Source Enthusiast'],
+  typeSpeed : 100,
+  backSpeed: 50,
+  delaySpeed : 90,
+  loop : true
+});
+
+/* Navbar scrolled */
+$(window).scroll(function() {
+  if ($(window).width() > 992) {
+    if ($(window).scrollTop() > 200) {
+      $(".navbar").addClass("navbar-scrolled");
+    }
+    else {
+      $(".navbar").removeClass("navbar-scrolled");
+    }
+  }
+});
+
+
+/* Navbar links active state on scroll */
+const select = (el, all = false) => {
+  el = el.trim()
+  if (all) {
+    return [...document.querySelectorAll(el)]
+  } else {
+    return document.querySelector(el)
+  }
+}
+
+const onscroll = (el, listener) => {
+  el.addEventListener('scroll', listener)
+}
+
+let navbarlinks = select('#navbar .nav-link', true)
+const navbarlinksActive = () => {
+  let position = window.scrollY + 60
+  navbarlinks.forEach(navbarlink => {
+    if (!navbarlink.hash) return
+    let section = select(navbarlink.hash)
+    if (!section) return
+    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+      navbarlink.classList.add('current')
+    } else {
+      navbarlink.classList.remove('current')
+    }
+  })
+}
+
+window.addEventListener('load', navbarlinksActive)
+onscroll(document, navbarlinksActive)
+
+
+/* Porjects */
+$(function() {
+	$(' #project-list > li ').each( function() { $(this).hoverdir(); } );
+});
+
+/* Footer */
+function validateEmail(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+    return false;
+  }
+  return true;
+}
+
+$(".alert-danger button").click(function() {
+  $(".alert-danger").hide();
+});
+$(".alert-warning button").click(function() {
+  $(".alert-warning").hide();
+});
+$(".alert-success button").click(function() {
+  $(".alert-success").hide();
+});
+
+function sendEmail() {
+  let name = $("#name").val();
+  let email = $("#email").val();
+  let subject = $("#subject").val();
+  let message = $("#message").val();
+
+  if (name == "" || email == "" || subject == "" || message == "") {
+    $(".alert-danger").show();
+    $(".alert-success").hide();
+    $(".alert-warning").hide();
+  } else if (validateEmail(email)) {
+    $(".alert-danger").show();
+    $(".alert-success").hide();
+    $(".alert-warning").hide();
+  } else {
+    let body = "Name: " + name + "<br>Email: " + email + "<br>Subject: " + subject + "<br>Message: " + message;
+
+    Email.send({
+    SecureToken:"c0d2eae6-34e2-4787-86f5-a6a0d78861e9",
+  
+    To : "sushantpatial18@gmail.com",
+    From : "patial.sushant12345@gmail.com",
+    Subject : "Portfolio - new message from " + name,
+    Body : body,
+    }).then(
+      message =>{
+        if(message=='OK'){
+          $(".alert-success").show();
+          $(".alert-danger").hide();
+          $(".alert-warning").hide();
+        }
+        else{
+          console.error (message);
+          $(".alert-warning").show();
+          $(".alert-danger").hide();
+          $(".alert-success").hide();
+        }
+      }
+    );
+  }
+}
+
+/* Getting year for footer */
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+/* Back to top button */
+let backtotop = select('.back-to-top')
+if (backtotop) {
+  const toggleBacktotop = () => {
+    if (window.scrollY > 100) {
+      backtotop.classList.add('active')
+    } else {
+      backtotop.classList.remove('active')
+    }
+  }
+  window.addEventListener('load', toggleBacktotop)
+  onscroll(document, toggleBacktotop)
+}
